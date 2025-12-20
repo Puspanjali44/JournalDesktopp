ab7ad2d1d779c93142f4aeb3e9fb030642632230
Puspanjali44 <adhipuspa5@gmail.com>
2025-12-20 20:27:46 +0545

    Add project files.

---
+++ MauiApp1/Components/NewFolder/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Likes { get; set; }
+    }
+}
